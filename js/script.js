b9dfd22fb051cdec91d427a71833c4ae5082a026
Ricardo Gava <Ricardoagava@gmail.com>
2020-09-03 02:05:43 -0300

    added explanation comments on the js file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
+//TAB MENU
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active');
+if (tabMenu.length && tabContent.length) {   //checking if on the actual page exists the classes
+    tabContent[0].classList.add('active');  //setting a defaut active class on the first item 
 
     function activeTab(index) {
-        tabContent.forEach((section) => {
+        tabContent.forEach((section) => {  //removing active from all sections and setting active based on the index
             section.classList.remove('active')
         })
         tabContent[index].classList.add('active')
     }
 
-    tabMenu.forEach((itemMenu, index) => {
+    tabMenu.forEach((itemMenu, index) => {  //setting a click event on every item and passing index as argument
         itemMenu.addEventListener('click', () => activeTab(index))
     });
 }
+
+//ACCORDION LIST
