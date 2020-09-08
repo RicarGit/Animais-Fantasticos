a185b3346bde8295574e879b7f34a71026ce1b10
Ricardo Gava <Ricardoagava@gmail.com>
2020-09-08 00:46:11 -0300

    added the function to smooth the scroll to the selected section

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ const activeClass = 'active';
 
 function initTabNav() {
     const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
     if (tabMenu.length && tabContent.length) {   //checking if on the actual page exists the classes
         tabContent[0].classList.add(activeClass);  //setting a defaut active class on the first item 
 
         function activeTab(index) {
             tabContent.forEach((section) => {  //removing active from all sections and setting active based on the index
-                section.classList.remove(activeClass)
+                section.classList.remove(activeClass);
             })
-            tabContent[index].classList.add(activeClass)
+            tabContent[index].classList.add(activeClass);
         }
 
         tabMenu.forEach((itemMenu, index) => {  //setting a click event on every item and passing index as argument
-            itemMenu.addEventListener('click', () => activeTab(index))
+            itemMenu.addEventListener('click', () => activeTab(index));
         });
     }
 }
@@ -35,8 +35,36 @@ function initAccordion() {
         }
 
         accordionList.forEach((dt) => {
-            dt.addEventListener('click', activeAccordion)
+            dt.addEventListener('click', activeAccordion);
         });
     }
 }
 initAccordion();
+
+//Internal Links Smooth Scroll
+function initSmoothScroll() {
+
+    const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        //const top = section.offsetTop;  //descobrindo o valor do topo do elemento
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+        });
+
+        /*alternative form
+        window.scrollTo({
+            top: top,
+            behavior: 'smooth',
+        })*/
+    }
+
+    internalLinks.forEach(link => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+initSmoothScroll();
